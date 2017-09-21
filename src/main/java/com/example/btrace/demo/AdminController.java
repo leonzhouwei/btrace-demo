b0a958391a5ccb3df7b2b12c163c681b6ed1329c
Wei Zhou <leonzhouwei@gmail.com>
2017-09-21 23:52:40 +0800

    refactor: tiny refine

--- src/main/java/com/example/btrace/demo/AdminController.java
+++ src/main/java/com/example/btrace/demo/AdminController.java
@@ -3,6 +3,13 @@ package com.example.btrace.demo;
 public class AdminController {
 
 	public String sayHello(String name, int age) {
+		try {
+			long rand = System.currentTimeMillis() % 100;
+			Thread.sleep(100 + rand);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "hello everyone";
 	}
 
--- src/main/java/com/example/btrace/demo/AdminControllerBTrace.java
+++ src/main/java/com/example/btrace/demo/AdminControllerBTrace.java
@@ -3,6 +3,7 @@ package com.example.btrace.demo;
 import static com.sun.btrace.BTraceUtils.println;
 
 import com.sun.btrace.annotations.BTrace;
+import com.sun.btrace.annotations.Duration;
 import com.sun.btrace.annotations.Kind;
 import com.sun.btrace.annotations.Location;
 import com.sun.btrace.annotations.OnMethod;
@@ -12,10 +13,12 @@ import com.sun.btrace.annotations.Return;
 public class AdminControllerBTrace {
 
 	@OnMethod(clazz = "com.example.btrace.demo.AdminController", method = "sayHello", location = @Location(Kind.RETURN) )
-	public static void sayHello(String name, int age, @Return String result) {
+	public static void sayHello(String name, int age, @Return String result, @Duration long duration) {
+		println("============================================================");
 		println("name: " + name);
 		println("age: " + age);
-		println(result);
+		println("@return: " + result);
+		println("@duration: " + duration / 1000000 + "ms");
 	}
 
 }
